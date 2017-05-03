c0d1d28ad4dea867c2634024372f4186321d01d0
Unknown <laurent.esteve@soprasteria.com>
2017-05-03 13:07:17 +0200

    Ajout Squelette

---
+++ src/main/java/agence/dao/ClientDao.java
@@ -0,0 +1,8 @@
+package agence.dao;
+
+import agence.model.Client;
+
+
+public interface ClientDao extends Dao<Client, Integer> {
+
+}
---
+++ src/main/java/agence/dao/ClientMoralDao.java
@@ -0,0 +1,21 @@
+package agence.dao;
+
+import java.util.List;
+
+import agence.model.Client;
+
+public class ClientMoralDao implements ClientDao {
+
+	@Override
+	public List<Client> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Client findById(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
